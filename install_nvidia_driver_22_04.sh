0661cc5568eb299a991fc2855e37569071942c3e
KontonGu <kontonpuku@gmail.com>
2025-05-11 23:33:58 +0000

    update

--- install_nvidia_driver_22_04.sh
+++ install_nvidia_driver_22_04.sh
@@ -1,12 +1,11 @@
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda-repo-ubuntu2204-12-8-local_12.8.0-570.86.10-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-8-local_12.8.0-570.86.10-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2204-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-8
+sudo apt-get install -y nvidia-driver-570
 
-sudo apt-get install -y cuda-drivers
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-6
+
 
 CUDA_PATH=/usr/local/cuda
 CUDA_PATH_LINE="export PATH=\$CUDA_PATH/bin:\$PATH"
